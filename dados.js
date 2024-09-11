65dcdab19f5c4f9f478fb0f15173125c13c8f2c7
ThiagoRickSnow <thiago.rickforever@gmail.com>
2024-09-11 01:11:59 -0300

    Atualizar o dados.js inclusão da tag "todos"

--- dados.js
+++ dados.js
@@ -4,69 +4,79 @@ const filmes = [
         sinopse: "A história da ascensão e queda de uma poderosa família mafiosa italiana em Nova York.",
         genero: "Crime, Drama",
         trailer: "https://www.youtube.com/watch?v=SaHZHU-44XA",
-        ano: "1972"
+        ano: "1972",
+        tags: ["todos"]
     },
     {
         titulo: "O Senhor dos Anéis: A Sociedade do Anel",
         sinopse: "Um hobbit encontra um anel mágico que pode dominar o mundo e se junta a uma comunidade de criaturas para destruí-lo.",
         genero: "Fantasia, Aventura",
         trailer: "https://www.youtube.com/watch?v=0i86oM1nHjM",
-        ano: "2001"
+        ano: "2001",
+        tags: ["todos"]
     },
     {
         titulo: "Pulp Fiction",
         sinopse: "Várias histórias interligadas sobre criminosos e outros personagens em Los Angeles.",
         genero: "Crime, Drama",
         trailer: "https://www.youtube.com/watch?v=tGpTpVyI_OQ",
-        ano: "1994"
+        ano: "1994",
+        tags: ["todos"]
     },
     {
         titulo: "O Iluminado",
         sinopse: "Um escritor, sua esposa e seu filho pequeno se mudam para um isolado hotel de montanha no inverno, onde eventos estranhos e horríveis começam a acontecer.",
         genero: "Terror, Suspense",
         trailer: "https://www.youtube.com/watch?v=zJK3fuMa9r8",
-        ano: "1980"
+        ano: "1980",
+        tags: ["todos"]
     },
     {
         titulo: "A Lista de Schindler",
         sinopse: "A história real de Oskar Schindler, um empresário alemão que salvou mais de mil judeus do Holocausto.",
         genero: "Drama, Histórico",
         trailer: "https://www.youtube.com/watch?v=RQfPCRUz6lw",
-        ano: "1993"
+        ano: "1993",
+        tags: ["todos"]
     },
     {
         titulo: "O Cavaleiro das Trevas",
         sinopse: "Batman enfrenta o Coringa, um anarquista psicopata que planeja causar o caos em Gotham City.",
         genero: "Ação, Tensão",
         trailer: "https://www.youtube.com/watch?v=EXeTwQWrcwY",
-        ano: "2008"
+        ano: "2008",
+        tags: ["todos"]
     },
     {
         titulo: "Interestelar",
         sinopse: "Um grupo de astronautas embarca em uma missão para encontrar um novo lar para a humanidade.",
         genero: "Ficção Científica",
         trailer: "https://www.youtube.com/watch?v=i6avfCqKcQo",
-        ano: "2014"
+        ano: "2014",
+        tags: ["todos"]
     },
     {
         titulo: "Parasita",
         sinopse: "Uma família pobre se infiltra na vida de uma rica família, expondo as desigualdades sociais.",
         genero: "Thriller, Drama",
         trailer: "https://www.youtube.com/watch?v=m4jfE-TxC24",
-        ano: "2019"
+        ano: "2019",
+        tags: ["todos"]
     },
     {
         titulo: "Vingadores: Ultimato",
         sinopse: "Os Vingadores se reúnem para reverter as ações de Thanos e restaurar o universo.",
         genero: "Ação, Aventura",
         trailer: "https://www.youtube.com/watch?v=PgrmbRID0eY",
-        ano: "2019"
+        ano: "2019",
+        tags: ["todos"]
     },
     {
         titulo: "Coringa",
         sinopse: "A origem do vilão mais famoso da DC Comics, explorando a transformação de Arthur Fleck em um psicopata.",
         genero: "Drama, Thriller",
         trailer: "https://www.youtube.com/watch?v=jfVTJm9NilA",
-        ano: "2019"
+        ano: "2019",
+        tags: ["todos"]
     }
 ];
